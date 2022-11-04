0e86ceec24d9574c0f7cd50f531c57f017e2edab
unknown <sjo506@naver.com>
2022-11-04 11:18:05 +0900

    face_gender_for_kiosk

--- kiosk_face.py
+++ kiosk_face.py
@@ -1,6 +1,8 @@
 import face_recognition
 import math
 import cv2
+from deepface import DeepFace
+
 
 def embedding_2_str(face_embedding, precision = 4, min_val = 1.0):
     # precision mean 0.000000 -> 6  아래 자리수 표현 즉 6일때 7칸 필요
@@ -70,10 +72,54 @@ def face_checker(cam_stream, face_size_theshold = 0.0):
                 print(top,right,bottom,left)
                 cv2.rectangle(frame, [left,top],[right,bottom],(0,255,0))
                 face_embedding = face_recognition.face_encodings(frame, [max_face_loc])
-                return 1, frame, embedding_2_str(face_embedding)
+                return 1, frame, embedding_2_str(face_embedding[0])
 
     else:
         return -1, None, ""
+
+
+def face_age_gender_checker(cam_stream, margin = 30):
+
+    def age_labeler(input):
+        if input < 6:
+            return 0
+        elif input < 38:
+            return 1
+        elif input < 60:
+            return 2
+        else:
+            return 3
+
+    ret, frame = cam_stream.read()
+    if ret:
+        H,W,_ = frame.shape
+        cam_size = H*W
+        face_loc = face_recognition.face_locations(frame)
+        # if len(face_loc) != 0:
+        #     top, right, bottom, left = face_loc[0]
+        #     cv2.rectangle(frame, [left,top],[right,bottom],(0,255,0))
+        # return 1,frame,""
+
+        if len(face_loc) == 0:
+            return -1, None,None
+        else:
+            answer = []
+            for top, right, bottom, left in face_loc:
+                im_t,im_b,im_l,im_r = max(0,top-margin),min(H,bottom+margin),max(0,left-margin),min(W,right+margin)
+
+                response=DeepFace.analyze(frame[im_t:im_b,im_l:im_r],
+                    actions=["gender","age"],
+                    enforce_detection=False,
+                    prog_bar= False,
+                    detector_backend="dlib")
+                answer.append([age_labeler(response["age"]), response["gender"]])
+
+            return len(answer),frame, answer
+
+
+    else:
+        return -1, None,None
+
 
 
 def get_single_face(cam):
@@ -88,7 +134,7 @@ def get_single_face(cam):
     else:
         return -1
 
-    return emb
+    return emb if emb != None else -1
 
 
 
@@ -100,10 +146,11 @@ if __name__ =='__main__':
     
 
     while cv2.waitKey(33) < 0:
-        ret, frame, emb = face_checker(cam)
-        print(ret)
+        ret, frame,answer = face_age_gender_checker(cam)
+        print(answer)
         if ret != -1:
             cv2.imshow("VideoFrame", frame)
+            cv2.waitKey(33)
             break
         #break
 
